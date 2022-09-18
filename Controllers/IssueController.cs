f5fcdf74fab243bd1a5fe3c44be2fc8124a8d32d
emmaroland32 <iyandasegunemmanuel@gmail.com>
2022-09-18 03:17:44 +0100

    Initial commit

---
+++ Controllers/IssueController.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IssueTracking.Data;
+using IssueTracking.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IssueTracking.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IssueController : ControllerBase
+
+    {
+        private readonly IssueDbContext _context;
+
+        public IssueController(IssueDbContext context) => _context = context;
+
+
+        /// <summary>
+        /// GET list of issues
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IEnumerable<Issue>> Get()
+        {
+            return await _context.Issues.ToListAsync();
+        }
+
+
+        /// <summary>
+        /// GET a single Issue
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(Issue), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var issue = await _context.Issues.FindAsync(id);
+            return issue == null ? NotFound() : Ok(issue);
+        }
+
+
+        /// <summary>
+        /// CREATE a new issue
+        /// </summary>
+        /// <param name="issue"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<IActionResult> Create(Issue issue)
+        {
+            await _context.Issues.AddAsync(issue);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetById), new { id = issue.Id }, issue);
+        }
+
+
+        /// <summary>
+        /// Update an Issue
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="issue"></param>
+        /// <returns></returns>
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Update(int id, Issue issue)
+        {
+            if (id != issue.Id) return BadRequest();
+
+            _context.Entry(issue).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
+        /// <summary>
+        /// DELETE an issue
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var issueToDelete = await _context.Issues.FindAsync(id);
+            if (issueToDelete == null) return NotFound();
+
+            _context.Issues.Remove(issueToDelete);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
---
+++ data/IssueDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using IssueTracking.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IssueTracking.Data
+{
+    public class IssueDbContext : DbContext
+    {
+        public IssueDbContext(DbContextOptions<IssueDbContext> options) :base(options)
+        {
+        }
+
+
+        public DbSet<Issue> Issues { get; set; }
+    }
+}
+
+/* To run migrations
+ * dotnet ef migrations add {createddatabase}
+ * dotnet ef migrations add createddatabase -o Data/Migrations
+ */
